f8781289307e1a9168056e5b42d75de10966445a
Yingray <yingray.lu@fuhu.com>
2016-10-21 18:05:17 +0800

    add redux files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './containers/App';
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import todoApp from './reducers'
 import './styles/index.css';
+// import App from './containers/App';
+import App from './components/App'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+let store = createStore(todoApp)
+
+render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+)

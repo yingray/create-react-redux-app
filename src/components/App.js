4c0e41002c13028b440e4eccb93fd1a265fa9115
Yingray <yingray.lu@fuhu.com>
2016-10-22 18:12:01 +0800

    updated project:

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 import React from 'react'
+import logo from '../images/logo.svg';
 import Footer from './Footer'
 import AddTodo from '../containers/AddTodo'
 import VisibleTodoList from '../containers/VisibleTodoList'
 
 const App = () => (
-	<div>
-		<AddTodo />
-		<VisibleTodoList />
-		<Footer />
+	<div className="App">
+		<div className="App-header">
+			<img src={logo} className="App-logo" alt="logo" />
+			<h2>Welcome to React-Redux</h2>
+		</div>
+		<p className="App-intro">
+			To get started, edit <code>src/containers/App.js</code> and save to reload.
+			<AddTodo />
+			<VisibleTodoList />
+			<Footer />
+		</p>
 	</div>
 )
 

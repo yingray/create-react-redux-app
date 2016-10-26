8d6b18c7cbf38bb7f5f8f393fd42ef5c0f24f71b
Yingray <yingray.lu@fuhu.com>
2016-10-27 00:41:07 +0800

    starbucks

--- src/reducers/weatherReducer.js
+++ src/reducers/weatherReducer.js
@@ -1,5 +1,3 @@
-import _ from 'lodash'
-
 const initialState = {
 	loading: false,
 	city: 'Initial',
@@ -11,21 +9,23 @@ const initialState = {
 const weatherReducer = (state = initialState, action) => {
 	switch (action.type) {
 		case 'WEATHER_SHOW_LOADING':
-			state.loading = true
-			return _.merge({}, state)
+			return Object.assign({}, state, {
+				loading: true
+			})
 
 		case 'WEATHER_SHOW_WEATHER':
 			const location = action.response.query.results.channel.location
 			const condition = action.response.query.results.channel.item.condition
-			state.loading = false
-			state.city = location.city
-			state.date = condition.date
-			state.temp = condition.temp
-			state.text = condition.text
-			return _.merge({}, state)
+			return Object.assign({}, state, {
+				loading: false,
+				city: location.city,
+				date: condition.date,
+				temp: condition.temp,
+				text: condition.text
+			})
 
 		default:
-			return _.merge({}, state)
+			return Object.assign({}, state, initialState)
 	}
 }
 
